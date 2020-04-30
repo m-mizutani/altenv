d302a69394db9e649dc611636b16ba7829047a67
Masayoshi Mizutani <mizutani@cookpad.com>
2020-05-01 00:45:47 +0900

    Fix return values of keychain methods for linux

--- keychain_linux.go
+++ keychain_linux.go
@@ -7,9 +7,9 @@ import (
 )
 
 func putKeyChainValues(envvars []*envvar, namespace string) error {
-	return fmt.Errorf("Keychain is not supported in the OS")
+	return nil, fmt.Errorf("Keychain is not supported in the OS")
 }
 
 func getKeyChainValues(namespace string) ([]*envvar, error) {
-	return fmt.Errorf("Keychain is not supported in the OS")
+	return nil, fmt.Errorf("Keychain is not supported in the OS")
 }
