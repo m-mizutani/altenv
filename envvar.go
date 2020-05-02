61541f7311241a0f6ed7915f24b0f5626678d691
Masayoshi Mizutani <mizutani@cookpad.com>
2020-05-02 19:33:12 +0900

    Refactor loadEnvVars

--- envvar.go
+++ envvar.go
@@ -31,84 +31,29 @@ func dumpEnvVars(w io.Writer, vars []*envvar) error {
 type fileOpen func(string) (io.ReadCloser, error) // based on os.Open
 type promptInput func(string) string              // based on prompter.Password
 
+type loadResult struct {
+	EnvVars []*envvar
+	Error   error
+}
+
 func loadEnvVars(config altenvConfig, ext ExtIOFunc) ([]*envvar, error) {
 	var envvars []*envvar
 
 	// Read environment variables
-	for _, f := range config.EnvFiles {
-		logger.WithField("path", f.Path).Debug("Read EnvFile")
-		vars, err := readEnvFile(f.Path, ext.OpenFunc)
-		if os.IsNotExist(err) && !f.IsRequired() {
-			logger.WithField("path", f.Path).Debug("EnvFile is not found, but ignore because not required")
-			continue
-		} else if err != nil {
-			return nil, errors.Wrapf(err, "Fail to read EnvFile %s", f.Path)
-		}
-
-		envvars = append(envvars, vars...)
+	results := []loadResult{
+		loadEnvFiles(config.EnvFiles, ext),
+		loadJSONFiles(config.JSONFiles, ext),
+		loadDefines(config.Defines),
+		loadKeychain(config.Keychains, config.KeychainServicePrefix, ext),
+		loadStdin(config.Stdin, ext),
+		loadPrompt(config.Prompt, ext),
 	}
 
-	for _, f := range config.JSONFiles {
-		logger.WithField("path", f.Path).Debug("Read JSON file")
-		vars, err := readJSONFile(f.Path, ext.OpenFunc)
-		if os.IsNotExist(err) && !f.IsRequired() {
-			logger.WithField("path", f.Path).Debug("JSON File is not found, but ignore because not required")
-			continue
-		} else if err != nil {
-			return nil, errors.Wrapf(err, "Fail to read JSON file %s", f.Path)
+	for _, result := range results {
+		if result.Error != nil {
+			return nil, result.Error
 		}
-		envvars = append(envvars, vars...)
-	}
-
-	for _, def := range config.Defines {
-		for _, vdef := range def.Vars {
-			logger.WithField("define", vdef).Debug("Set temp variables")
-			v, err := parseDefine(vdef)
-			if err != nil {
-				return nil, err
-			}
-			envvars = append(envvars, v)
-		}
-	}
-
-	for _, namespace := range config.Keychains {
-		args := getKeyChainValuesArgs{
-			namespace:     namespace,
-			servicePrefix: config.KeychainServicePrefix,
-			queryItem:     ext.KeychainQueryItem,
-		}
-		vars, err := getKeyChainValues(args)
-		if err != nil {
-			return nil, err
-		}
-		envvars = append(envvars, vars...)
-	}
-
-	switch config.Stdin {
-	case "json":
-		vars, err := parseJSONFile(ext.Stdin)
-		if err != nil {
-			return nil, errors.Wrap(err, "Fail to parse JSON format from stdin")
-		}
-		envvars = append(envvars, vars...)
-	case "env":
-		vars, err := parseEnvFile(ext.Stdin)
-		if err != nil {
-			return nil, errors.Wrap(err, "Fail to parse EnvFile format from stdin")
-		}
-		envvars = append(envvars, vars...)
-	case "":
-		// nothing to do
-	default:
-		return nil, fmt.Errorf("Invalid input format: `%s`", config.Stdin)
-	}
-
-	if config.Prompt != "" {
-		value := ext.InputFunc(fmt.Sprintf("Enter %s value", config.Prompt))
-		envvars = append(envvars, &envvar{
-			Key:   config.Prompt,
-			Value: value,
-		})
+		envvars = append(envvars, result.EnvVars...)
 	}
 
 	// Check overwrite
@@ -139,4 +84,116 @@ func loadEnvVars(config altenvConfig, ext ExtIOFunc) ([]*envvar, error) {
 	}
 
 	return newVars, nil
+}
+
+func loadEnvFiles(envFiles []*envFileConfig, ext ExtIOFunc) loadResult {
+	var envvars []*envvar
+
+	for _, f := range envFiles {
+		logger.WithField("path", f.Path).Debug("Read EnvFile")
+		vars, err := readEnvFile(f.Path, ext.OpenFunc)
+		if os.IsNotExist(err) && !f.IsRequired() {
+			logger.WithField("path", f.Path).Debug("EnvFile is not found, but ignore because not required")
+			continue
+		} else if err != nil {
+			return loadResult{nil, errors.Wrapf(err, "Fail to read EnvFile %s", f.Path)}
+		}
+
+		envvars = append(envvars, vars...)
+	}
+
+	return loadResult{envvars, nil}
+}
+
+func loadJSONFiles(jsonFiles []*jsonFileConfig, ext ExtIOFunc) loadResult {
+	var envvars []*envvar
+
+	for _, f := range jsonFiles {
+		logger.WithField("path", f.Path).Debug("Read JSON file")
+		vars, err := readJSONFile(f.Path, ext.OpenFunc)
+		if os.IsNotExist(err) && !f.IsRequired() {
+			logger.WithField("path", f.Path).Debug("JSON File is not found, but ignore because not required")
+			continue
+		} else if err != nil {
+			return loadResult{nil, errors.Wrapf(err, "Fail to read JSON file %s", f.Path)}
+		}
+		envvars = append(envvars, vars...)
+	}
+
+	return loadResult{envvars, nil}
+}
+
+func loadDefines(defines []*defineConfig) loadResult {
+	var envvars []*envvar
+
+	for _, def := range defines {
+		for _, vdef := range def.Vars {
+			logger.WithField("define", vdef).Debug("Set temp variables")
+			v, err := parseDefine(vdef)
+			if err != nil {
+				return loadResult{nil, err}
+			}
+			envvars = append(envvars, v)
+		}
+	}
+
+	return loadResult{envvars, nil}
+}
+
+func loadKeychain(keychains []string, servicePrefix string, ext ExtIOFunc) loadResult {
+	var envvars []*envvar
+
+	for _, namespace := range keychains {
+		args := getKeyChainValuesArgs{
+			namespace:     namespace,
+			servicePrefix: servicePrefix,
+			queryItem:     ext.KeychainQueryItem,
+		}
+		vars, err := getKeyChainValues(args)
+		if err != nil {
+			return loadResult{nil, err}
+		}
+		envvars = append(envvars, vars...)
+	}
+
+	return loadResult{envvars, nil}
+}
+
+func loadStdin(stdinFmt string, ext ExtIOFunc) loadResult {
+	var envvars []*envvar
+
+	switch stdinFmt {
+	case "json":
+		vars, err := parseJSONFile(ext.Stdin)
+		if err != nil {
+			return loadResult{nil, errors.Wrap(err, "Fail to parse JSON format from stdin")}
+		}
+		envvars = append(envvars, vars...)
+	case "env":
+		vars, err := parseEnvFile(ext.Stdin)
+		if err != nil {
+			return loadResult{nil, errors.Wrap(err, "Fail to parse EnvFile format from stdin")}
+		}
+		envvars = append(envvars, vars...)
+	case "":
+		// nothing to do
+	default:
+		return loadResult{nil, fmt.Errorf("Invalid input format: `%s`", stdinFmt)}
+	}
+
+	return loadResult{envvars, nil}
+}
+
+func loadPrompt(prompt string, ext ExtIOFunc) loadResult {
+	var envvars []*envvar
+
+	if prompt != "" {
+		value := ext.InputFunc(fmt.Sprintf("Enter %s value", prompt))
+		envvars = append(envvars, &envvar{
+			Key:   prompt,
+			Value: value,
+		})
+	}
+
+	return loadResult{envvars, nil}
 }
